a2e369b8fdfe1e8be154e8552ce7e1ef0433b863
Joris_Fonck <fonckjor@gmail.com>
2017-11-21 17:03:55 +0100

    Modification front & dépendances bower.

--- iodeman/src/main/resources/public/app/scripts/controllers/main.js
+++ iodeman/src/main/resources/public/app/scripts/controllers/main.js
@@ -8,33 +8,35 @@
  * Controller of the publicApp
  */
 angular.module('publicApp')
-.controller('MainCtrl', function ($scope, $http, backendURL, Auth, $sessionStorage, $rootScope, $timeout) {
+.controller('MainCtrl', function ($scope, $http, $log, backendURL, Auth, $sessionStorage, $rootScope, $timeout) {
 	$.material.init();
 	$scope.connected = false;
-	
-	$http.get(backendURL + 'plannings/exported')
-	.success(function(data) {
+
+	$http.get(backendURL + 'plannings/exported').success(function(data) {
 		$scope.exported = data.keys;
 	});
 
 	$http.get(backendURL + 'user').success(function(data) {
-		console.log(data);
-		$scope.user = data;
+	    $scope.user = data;
 	});
-		
+
 	$http.get(backendURL + 'planning/list').success(function(data) {
+        $log.debug(data);
+
 		$scope.plannings = data;
 		$scope.connected = true;
+
 		$("#home-spinner").remove();
 	});
 
 	$scope.closeHomeInfo = function() {
 		$("#home-info").fadeOut(300, function() { $(this).remove(); });
 	};
-	
+
 	$scope.remove = function(id) {
-		console.log(backendURL + 'planning/'+id+'/delete');
-		$http.get(backendURL + 'planning/'+id+'/delete').success(function (data) {
+		$log.debug(backendURL + 'planning/'+id+'/delete');
+
+		$http.get(backendURL + 'planning/'+id+'/delete').success(function () {
 			for(var i = $scope.plannings.length - 1; i >= 0; i--) {
 			    if($scope.plannings[i].id === id) {
 			    	$scope.plannings.splice(i, 1);
